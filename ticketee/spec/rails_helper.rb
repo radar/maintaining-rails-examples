3624019214c84396bc51e7e050703139dc5592bb
Ryan Bigg <ryanbigg@cultureamp.com>
2018-06-17 15:45:06 +1000

    Chapter 2: Initial Application Setup

---
+++ ticketee/spec/rails_helper.rb
@@ -0,0 +1,67 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The following line is provided for convenience purposes. It has the downside
+# of increasing the boot-up time by auto-requiring all files in the support
+# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# require only the support files necessary.
+#
+# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  def db
+    Sequel::DATABASES.first
+  end
+
+  def migrations_must_be_run!
+    raise "Migrations need to be run. Run this command: DATABASE_URL='postgres://localhost/cycad_test' rake db:migrate"
+  end
+
+  config.before(:all) do
+    migrations = db[:schema_migrations].order(Sequel.desc(:filename))
+    migrations_must_be_run! if migrations.empty?
+
+    last_migration_in_db = migrations.first[:filename]
+    last_migration_in_fs = File.basename(Dir[Rails.root + "db/migrate/*"].sort.last)
+    if last_migration_in_fs != last_migration_in_db
+      migrations_must_be_run!
+    end
+  end
+
+  config.before do
+    db[*(db.tables - [:schema_migrations])].truncate
+  end
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, :type => :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+end
