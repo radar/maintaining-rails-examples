214ca9d4022faae884fb6b724a3ec005192034ec
Ryan Bigg <ryanbigg@cultureamp.com>
2018-06-17 15:45:06 +1000

    Chapter 2: Initial Application Setup

--- ticketee/spec/rails_helper.rb
+++ ticketee/spec/rails_helper.rb
@@ -45,6 +45,7 @@ RSpec.configure do |config|
   config.before do
     db[*(db.tables - [:schema_migrations])].truncate
   end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
