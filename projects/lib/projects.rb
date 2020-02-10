54d189b31f5de6f227931087ef5693542e6e0d7b
Piotr Solnica <piotr.solnica@gmail.com>
2020-02-10 22:41:32 +0100

    Add settings

--- projects/lib/projects.rb
+++ projects/lib/projects.rb
@@ -2,9 +2,12 @@ require 'bundler'
 Bundler.setup
 
 require 'dry/system/container'
+require 'dry/system/components'
 
 module Projects
   class Application < Dry::System::Container
+    use :env, inferrer: proc { ENV.fetch('PROJECTS_ENV', 'development') }
+
     configure do |config|
       config.root = File.expand_path('..', __dir__)
       config.default_namespace = 'projects'
