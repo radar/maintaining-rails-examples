b5df28e87642c9d41971bb6ae12e1e65626ae2c9
Ryan Bigg <me@ryanbigg.com>
2020-10-20 12:41:54 +1100

    End of Chapter 3

---
+++ ticketee/app/controllers/projects/projects_controller.rb
@@ -0,0 +1,23 @@
+module Projects
+  class ProjectsController < ApplicationController
+    def new
+      @project = Projects::Project.new
+    end
+
+    def create
+      repo.create(project_params)
+      flash[:notice] = "Project has been created."
+      redirect_to action: :index
+    end
+
+    private
+
+    def project_params
+      params.require(:project).permit(:name).to_h.symbolize_keys
+    end
+
+    def repo
+      ProjectRepository.new(rom)
+    end
+  end
+end
